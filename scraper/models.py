a617bcd9a61701e3f5769b1ac82ff15a9b144c30
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2023-02-13 17:02:47 +0200

    Delete scraper directory

--- scraper/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class ScrapedData(models.Model):
-    url = models.URLField(max_length=200)
-    html = models.TextField()
-    
-    class Meta:
-        app_label = 'scraper'
-
